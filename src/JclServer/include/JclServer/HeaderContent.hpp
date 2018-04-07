851f002bce6f0b2e7367846d981f6cc4ea695d55
Cassie E Nicol <cassieenicol@gmail.com>
2018-04-07 05:51:43 -0500

    content classes

---
+++ src/JclServer/include/JclServer/HeaderContent.hpp
@@ -0,0 +1,43 @@
+// HeaderContent.hpp
+//
+// <one line to give the program's name and a brief idea of what it does.>
+// Copyright (C) 2018 Cassie E Nicol
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// SPDX-License-Identifier:	GPL-3.0
+
+#ifndef HeaderContent_INCLUDED
+#define HeaderContent_INCLUDED
+
+#include <JclServer/PageContent.hpp>
+
+
+namespace jcl {
+
+class HeaderContent : public PageContent
+{
+public:
+    HeaderContent(Page& page)
+        : PageContent("Header", page)
+    {
+    }
+    ~HeaderContent();
+    
+    std::ostream& write(std::ostream& os) const;
+
+};
+
+}
+#endif // HeaderContent_INCLUDED
