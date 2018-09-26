6d07d77d4f1c4ed7758a0ad906c77997e7bce7d3
Cassie Nicol <cassieenicol@gmail.com>
2018-09-26 17:17:57 -0500

    Updating for Ubuntu 18.04

--- src/JclServer/src/Page.cpp
+++ src/JclServer/src/Page.cpp
@@ -21,6 +21,8 @@
 #include <JclServer/Page.hpp>
 #include <JclServer/PageContent.hpp>
 #include <JclServer/HeadContent.hpp>
+#include <JclServer/HeaderContent.hpp>
+#include <JclServer/FooterContent.hpp>
 #include <JclServer/TextContent.hpp>
 
 #include <Poco/Util/Application.h>
@@ -34,76 +36,86 @@
 namespace jcl {
     using namespace std;
     using namespace Poco::Net;
-    
+
     using Poco::Net::HTTPServerRequest;
     using Poco::Net::HTTPServerResponse;
     using Poco::NullPointerException;
     using Poco::Net::NameValueCollection;
 
     Page::Page(const std::string &name, Poco::Net::HTTPServerRequest &request, Poco::Net::HTTPServerResponse &response)
-            : _name(name)
-            , _request(request)
-            , _response(response)
-            , _formData()
-            , _content()
-            , _logger(Poco::Logger::get("Page"))
+            : _name(name), _request(request), _response(response), _formData(), _content(),
+              _logger(Poco::Logger::get("Page"))
     {
+        string level = Poco::Util::Application::instance().config().getString("application.log_level", "none");
+        _logger.setLevel(level);
+
         _logger.trace(__PRETTY_FUNCTION__);
 
-        // Set the default page title.
-        if (!_formData.has("page.title")) {
-            _formData.set("page.title", "jclchat");
+        // Set the default getPage title.
+        if (!_formData.has("getPage.title")) {
+            _formData.set("getPage.title", "jclchat");
         }
 
         // Add in the head section
-        add(new TextContent("doctype", "<!DOCTYPE html>", *this));
-        add(new TextContent("html", "<html>", *this));
-        add(new HeadContent(*this));
-        add(new TextContent("body", "<body>", *this));
+        add(make_shared<TextContent>("doctype", "<!DOCTYPE html>"));
+        add(make_shared<TextContent>("html", "<html>"));
+        add(make_shared<HeadContent>());
+        add(make_shared<TextContent>("body", "<body>"));
+        add(make_shared<HeaderContent>());
     }
 
-    Page::~Page()
-    {
-    }
-
-    void Page::send()
-    {
+    Page::~Page() {
         _logger.trace(__PRETTY_FUNCTION__);
+
+    }
+
+    void Page::send() {
+        _logger.trace(__PRETTY_FUNCTION__ + _name);
+
         // Show contents of _formData
         _logger.debug("_formData contents");
-        for(auto it : _formData ) {
+        for (auto it : _formData) {
             _logger.debug("\t" + it.first + ": " + it.second);
         }
+
+        // Give and OK response, and set the getPage content to HTML
         _response.setStatus(HTTPResponse::HTTP_OK);
         _response.setContentType("text/html");
 
-        ostream& out = _response.send();
+        ostream &out = _response.send();
 
-        add(new TextContent("/body", "</body>", *this));
-        add(new TextContent("/html", "</html>", *this));
+        add(make_shared<FooterContent>());
+        add(make_shared<TextContent>("/body", "</body>"));
+        add(make_shared<TextContent>("/html", "</html>"));
 
-        for(auto content: _content) {
+        for (auto content: _content) {
             content->write(out);
         }
     }
 
-    void Page::add(PageContent* content)
-    {
-        shared_ptr<PageContent> p(content);
-        _content.push_back( p );
+    void Page::add(const std::shared_ptr<PageContent> &content) {
+        _logger.trace(__PRETTY_FUNCTION__ + content->getName());
+
+        content->setPage(this);
+
+        _content.push_back(content);
     }
 
     // Has a odd side-effect of inserting at the end if the named element is not found. 
-    void Page::addAfter(const string& name, PageContent* content)
-    {
-        auto it
-            = find_if(_content.begin(), _content.end(),
-                      [&name](shared_ptr<PageContent> e)->bool{ return e->name() == name; } );
+    void Page::addAfter(const string &name, std::shared_ptr<PageContent> &content) {
+        _logger.trace(__PRETTY_FUNCTION__ + content->getName());
 
         shared_ptr<PageContent> p(content);
 
-        if(it == _content.end()) {
-            _content.push_back( p );
+        p->setPage( this );
+
+
+        auto it
+                = find_if(_content.begin(), _content.end(),
+                          [&name](shared_ptr<PageContent> e) -> bool { return e->getName() == name; });
+
+        if (it == _content.end()) {
+            _content.push_back(p);
         } else {
             ++it;
             _content.insert(it, p);
